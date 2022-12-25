80f41e76ab4ec38697e66e428f8e59d491d9f5ab
jellyfishimn <jellyfishimn@proton.me>
2022-12-26 02:04:35 +0300

    update consumer for messages from telegram-bot-server

--- src/main/java/org/asap/notes_server/model/entity/Note.java
+++ src/main/java/org/asap/notes_server/model/entity/Note.java
@@ -1,13 +1,23 @@
 package org.asap.notes_server.model.entity;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor(staticName = "of")
+@NoArgsConstructor
 public class Note extends BaseEntity {
 
-    private String description;
+    private String who;
+
+    private LocalDateTime time;
+
+    private String message;
 }
--- src/main/java/org/asap/notes_server/service/NoteConsumer.java
+++ src/main/java/org/asap/notes_server/service/NoteConsumer.java
@@ -2,16 +2,23 @@ package org.asap.notes_server.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.asap.notes_server.model.entity.Note;
+import org.asap.notes_server.repository.NoteRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class NoteConsumer {
 
-    @RabbitListener
+    private final NoteRepository noteRepository;
+
+    @RabbitListener(queues = "notes-queue")
     public void process(final String message) {
-        System.out.printf("Received from: %s ", new String(message.getBytes()));
+        noteRepository.save(Note.of("anonymous", LocalDateTime.now(), message));
+        log.debug("Received message from telegram-bot-server: {}", message);
     }
 }
